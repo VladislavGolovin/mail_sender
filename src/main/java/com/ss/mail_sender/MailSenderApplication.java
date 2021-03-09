457461b8aea5a4ec4ea4dc21380657650711b094
vechkitov <vechkitov@bk.ru>
2021-03-09 09:45:59 +0200

    Classes was added to model and serveces package

--- src/main/java/com/ss/mail_sender/MailSenderApplication.java
+++ src/main/java/com/ss/mail_sender/MailSenderApplication.java
@@ -2,12 +2,28 @@ package com.ss.mail_sender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class MailSenderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailSenderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MailSenderApplication.class, args);
+    }
 
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("MailSender-");
+        executor.initialize();
+        return executor;
+    }
 }
---
+++ src/main/java/com/ss/mail_sender/model/Email.java
@@ -0,0 +1,8 @@
+package com.ss.mail_sender.model;
+
+public class Email {
+
+    private String to;
+    private String subject;
+    private String body;
+}
