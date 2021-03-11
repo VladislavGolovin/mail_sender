74aaf8cd2854290b874c239a87539a0f279f7e4e
Vladislav Golovin <vlad.golovin@simple-solution.biz>
2021-03-11 17:03:13 +0300

    Control Point

--- src/main/java/com/ss/mailsender/DataValidator.java
+++ src/main/java/com/ss/mailsender/DataValidator.java
@@ -3,7 +3,9 @@ package com.ss.mailsender;
 import com.ss.mailsender.libs.CsvParser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,28 +23,56 @@ public class DataValidator {
     // creating list for invalid data
     List <String[]> notValidData = new ArrayList<>();
 
+    Map <String, List<String[]>> lists = new HashMap();
+
     // settings
     int emailPosition = 0;
     int validNumberOfData = 3;
 
-    // as result we are returning list of invalid rows
-    public List<String[]> dataValidator(String fileName){
+    // as result we are returning map of valid and invalid rows
+    public Map <String, List<String[]>> dataValidator(String fileName){
 
         // creating object of CsvParser
         CsvParser cp = new CsvParser();
 
         notSortedData = cp.csvParser(fileName);
 
+        String message;
+
         // watching rows(lines) one by one. If row contains valid number of elements
         // and emailchecker returns "true" - adding row in validList
         for (String[] data: notSortedData) {
             if (data.length == validNumberOfData && emailChecker(data[emailPosition]) == true){
                 validData.add(data);
-            } else {
-                notValidData.add(data);
+
+                // if row`s length is invalid, we are adding error message to the first column
+            } else if (data.length != validNumberOfData && emailChecker(data[emailPosition]) == true){
+                message = "row`s lenght not valid";
+                String[] invalidData = new String[data.length + 1];
+                invalidData[0] = message;
+                System.arraycopy(data, 1, invalidData, 1, data.length);
+                notValidData.add(invalidData);
+
+                // if row`s length and email is invalid, we are adding error message to the first column
+            } else if (data.length != validNumberOfData && emailChecker(data[emailPosition]) != true){
+                message = "row`s lenght not valid and email is invalid!";
+                String[] invalidData = new String[data.length + 1];
+                invalidData[0] = message;
+                System.arraycopy(data, 1, invalidData, 1, data.length);
+                notValidData.add(invalidData);
+
+                // if email is invalid, we are adding error message to the first column
+            } else if (data.length == validNumberOfData && emailChecker(data[emailPosition]) != true){
+                message = "email is invalid!";
+                String[] invalidData = new String[data.length + 1];
+                invalidData[0] = message;
+                System.arraycopy(data, 1, invalidData, 1, data.length);
+                notValidData.add(invalidData);
             }
         }
-        return notValidData;
+        lists.put("valid", validData);
+        lists.put("invalid", notValidData);
+        return lists;
     }
 
     // checking for email validation
