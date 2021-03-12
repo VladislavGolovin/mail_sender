9258e7d6df340fd73471d7acc8a97cd1787d2113
vechkitov <vechkitov@bk.ru>
2021-03-12 12:15:03 +0200

    email address mock was changed

--- src/test/java/com/ss/mailsender/services/EmailServiceTest.java
+++ src/test/java/com/ss/mailsender/services/EmailServiceTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmailServiceTest {
 
-    private static final String SEND_TO = "";
+    private static final String SEND_TO = "a@a.a";
 
     @Autowired
     private EmailService emailService;
